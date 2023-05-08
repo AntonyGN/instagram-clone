5809635ba2e68a0769ff85d9016f3f6bc5fbb92e
Antony Githendu <antonygithendu@gmail.com>
2023-05-08 16:33:15 +0300

    initial commit

---
+++ Desktop/freeCodeGram/resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-3">
+        <img src="	https://instagram.fnbo10-1.fna.fbcdn.net/v/t51.288…p3dA08ZW1Vn_q8f5VplZ2Q&oe=645D33B7&_nc_sid=8fd12b" alt="">
+
+    </div>
+
+
+    <div class="col-9 pt-5">
+        <div><h1>Antony Githendu</h1></div>
+        <div class="d-flex">
+            <div class="pr-5"><strong>153</strong> posts</div>
+            <div class="pr-5"><strong>23k</strong> followers</div>
+            <div class="pr-5"><strong>212</strong> following</div>
+        </div>
+
+        <div class="pt-4 font-weight-bold">Antony Githendu</div>
+        <div>I am always eager to share my knowledge and expertise with others, and I take great pride in working collaboratively with my fellow developers to achieve shared goals.</div>
+        <div><a href="#">www.githendu.netlify.app</a></div>
+    </div>
+  </div>
+</div>
+@endsection
